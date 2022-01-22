84c3c8649cc9676d29a646a6a25712374c4a3cac
toby1a05 <32935616+toby1a05@users.noreply.github.com>
2022-01-22 01:29:43 -0800

    dev

--- src/com/test/springboot/myCoachApp.java
+++ src/com/test/springboot/myCoachApp.java
@@ -6,9 +6,9 @@ public class myCoachApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"); /*get the context from applicationContext.xml*/
 		
-		Coach coach = context.getBean("myCoach", Coach.class);
+		Coach coach = context.getBean("myCoach", Coach.class);/*get spring bean object*/
 		
 		System.out.println(coach.doWorkout());
 	}
